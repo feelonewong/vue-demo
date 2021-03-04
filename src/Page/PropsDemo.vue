956a8735d72be066090eba983b1c378db934bb26
feelonewong <feelonewong@gmail.com>
2021-03-04 18:15:51 +0800

    props

---
+++ src/Page/PropsDemo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      Post.id:{{post.id}} , Post.title: {{post.title}}<br>
+      propsString:{{propsString}}
+      <button @click="handleChange">check propsString</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        propsString:{
+            type:String
+        },
+        post:{
+            type: Object,
+        },
+        list:{
+            type:Object,
+            default:()=>{
+                return {
+                    message:"hello"
+                }
+            }
+        },
+        propsChange:{
+            type: Function
+        }
+    },
+    methods:{
+        handleChange(){
+            console.log('child',this.propsString);
+            this.propsChange(this.propsString==="success"?"warning":"success")
+        }
+    },
+    mounted(){
+    }
+}
+</script>
+
+<style>
+
+</style>
